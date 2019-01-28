d9d22d45e4a845ecc56ef5bd731d4d9cb3c4a636
wonder woman <rdelquiam@gmail.com>
2019-01-28 10:33:18 -0800

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,17 @@
 var mysql = require("mysql");
 
 var connection;
-if(process.env.JAWSDB_URL){
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWDB_URL);
-}else{
+} else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -23,4 +24,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+
 module.exports = connection;
