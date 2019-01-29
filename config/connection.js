a602218ab1ec347e9e47447c493978c4817ac55c
wonder woman <rdelquiam@gmail.com>
2019-01-29 10:33:04 -0800

    updated db

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
@@ -15,7 +15,7 @@ if (process.env.JAWSDB_URL) {
 };
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
